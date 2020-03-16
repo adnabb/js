24117b2d0f1c854f581349d1c4683e5058e0bf7a
chenshiyu <chenshiyu@ygsoft.com>
2020-03-16 11:11:34 +0800

    feat:bing绑定增加老语法

--- bind/index.js
+++ bind/index.js
@@ -1,4 +1,23 @@
-const bind = function (oThis, ...args) {
+// 老语法
+var slice = Array.prototype.slice;
+var bind = function(oThis) {
+  var fn = this;
+  var args = slice.call(arguments, 1);
+
+  if (typeof fn !== 'function') throw new Error('bind只能绑定函数');
+
+  function result() {
+    var args2 = slice.call(arguments);
+    return fn.apply(result.prototype.isPrototypeOf(this) ? this : oThis, args.concat(args2));
+  };
+
+  result.prototype = fn.prototype;
+
+  return result;
+};
+
+// 使用es6语法
+const _bind = function (oThis, ...args) {
   const fn = this;
   const resultFn = function (...args2) {
     const that = this instanceof resultFn ? this : oThis;
@@ -7,7 +26,7 @@ const bind = function (oThis, ...args) {
   resultFn.prototype = fn.prototype;
 
   return resultFn;
-}
+};
 
 // Function.proptotype.bind不允许被改写，这里使用bind2
 if (!Function.prototype.bind2) Function.prototype.bind2 = bind;
--- bind/index.test.js
+++ bind/index.test.js
@@ -8,42 +8,45 @@ test5('bing返回的函数允许new操作');
 test6('new的函数可以使用绑定的函数的prototype属性');
 test7('bind绑定的对象和new的对象都是同一个原型链');
 
+// 由于断言错误代码还是会继续运行，所以在每一个测试用例前打印message
+// 如果出现断言错误，就可以根据message定位出错的地方
 function test1(message) {
+  console.log(message);
   console.assert(bind_c, 'exsit');
   console.assert(typeof bind_c === 'function', 'function');
   console.assert(bind_c === Function.prototype.bind2, 'prototype');
-  console.log(message);
 }
 
 function test2(message) {
+  console.log(message);
   const fn = function () { return this; };
   const obj = { name: 'vivi' };
   const newFn = fn.bind2(obj);
   console.assert(newFn() === obj, 'this');
-  console.log(message);
 }
 
 function test3(message) {
+  console.log(message);
   const fn = function (p1, p2) { return [this, p1, p2]; };
   const obj = { name: 'vivi' };
   const newFnResult = fn.bind2(obj, 123, 234)();
   console.assert(newFnResult[0] === obj, 'this');
   console.assert(newFnResult[1] === 123, 'p1');
   console.assert(newFnResult[2] === 234, 'p2');
-  console.log(message);
 }
 
 function test4(message) {
+  console.log(message);
   const fn = function (p1, p2) { return [this, p1, p2]; };
   const obj = { name: 'vivi' };
   const newFnResult = fn.bind2(obj)(123, 234);
   console.assert(newFnResult[0] === obj, 'this');
   console.assert(newFnResult[1] === 123, 'p1');
   console.assert(newFnResult[2] === 234, 'p2');
-  console.log(message);
 }
 
 function test5(message) {
+  console.log(message);
   const fn = function (p1, p2) { return [this, p1, p2]; };
   const obj = { name: 'vivi' };
   const newFn = fn.bind2(obj, 123, 234);
@@ -51,10 +54,10 @@ function test5(message) {
   console.assert(newFnResult[0] instanceof newFn, 'this');
   console.assert(newFnResult[1] === 123, 'p1');
   console.assert(newFnResult[2] === 234, 'p2');
-  console.log(message);
 }
 
 function test6(message) {
+  console.log(message);
   // 由于new的时候会返回函数自身，所以 这里不能有返回值
   const fn = function (p1, p2) { this.p1 = p1; this.p2 = p2; };
   fn.prototype.sayHi = function() { console.log('hi'); };
@@ -67,10 +70,10 @@ function test6(message) {
   console.assert(newFnResult instanceof newFn, 'this');
   console.assert(newFnResult.p1 === 123, 'p1');
   console.assert(newFnResult.p2 === 234, 'p2');
-  console.log(message);
 }
 
 function test7(message) {
+  console.log(message);
   // 由于new的时候会返回函数自身，所以 这里不能有返回值
   const fn = function (p1, p2) { this.p1 = p1; this.p2 = p2; };
   fn.prototype.sayHi = function() { console.log('hi'); };
@@ -83,5 +86,4 @@ function test7(message) {
   console.assert(newFnResult instanceof newFn, 'this');
   console.assert(newFnResult.p1 === 123, 'p1');
   console.assert(newFnResult.p2 === 234, 'p2');
-  console.log(message);
 }
